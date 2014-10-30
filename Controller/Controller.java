268e8f934ad3a1f0e3516387641c4cd92ee328d1
Yolof <slask@yolof.se>
2014-10-30 10:56:41 +0100

    Create Controller.java

---
+++ Controller/Controller.java
@@ -0,0 +1,25 @@
+package Controller;
+
+import DataAccessLayer.DAL;
+
+public class Controller {
+
+	DAL dal;
+	
+	public Controller() {
+	
+		DAL dal = new DAL();
+		
+	}
+	
+	public void RegisterStudent() {
+
+//		executeDAL.registerStudent(student);
+		
+	}
+	
+	public void RegisterCourse() {
+		
+	}
+	
+}
