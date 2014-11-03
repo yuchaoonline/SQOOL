d9667c25f0347a8af222df00c7e3b93ca4bee4fa
dennisdembo <dennis.olsson@mail.com>
2014-11-03 18:29:11 +0100

    Create föremailotele.java

---
+++ Controller/föremailotele.java
@@ -0,0 +1,8 @@
+	public void registerStudentEmail(String eMail, String socNmbr) throws SQLException {
+		dal.registerStudentEmail(eMail, socNmbr);
+	}
+	
+	public void registerStudentPhone(String telNmbr, String socNmbr) throws SQLException {
+		dal.registerStudentPhone(telNmbr, socNmbr);
+	}
+	
