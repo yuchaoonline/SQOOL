f736dd7ca80df88891752bb065e45075192f059d
Yolof <slask@yolof.se>
2014-11-10 23:44:50 +0100

    Create ExecuteProgram.java

---
+++ SQOOL/default-package/ExecuteProgram.java
@@ -0,0 +1,8 @@
+import java.sql.SQLException;
+import view.MainGUI;
+
+public class ExecuteProgram {	
+	public static void main(String[] args) throws SQLException {
+		new MainGUI().setVisible(true);
+	}
+}
